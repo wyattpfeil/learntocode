59fa8fd8261f5913f0eef59fa4b8203f045219df
Wyatt Pfeil <wyatt.pfeil@gmail.com>
2019-01-28 12:14:02 -0500

    tidy up development script

--- scripts/development.sh
+++ scripts/development.sh
@@ -5,4 +5,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 alias deploy='$DIR/deploy.sh'
-deploy; fswatch -o . | while read f; do deploy; done
+
+# always do a deploy on first run
+deploy
+
+# deploy when there are any file changes
+fswatch --one-per-batch . | while read filename;
+do
+	#echo $filename $event
+	deploy;
+done
